247e95cf208d241a9868785640f65c7f800d832c
Ouattara Youssouf <webwatson92@gmail.com>
2016-09-09 23:44:46 +0000

    save des trois pages cree

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/help'
+
+  root 'pages#home'
+  get '/about' => 'pages#about'
+  get '/help' => 'pages#help'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
